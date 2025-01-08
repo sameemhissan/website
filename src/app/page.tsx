08dd8e3ec17dac654246cef7f51b68a881d82b22
cosmicsarthak <sarthakmohanty200@gmail.com>
2025-01-09 02:37:39 +0530

    contact check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,7 +166,7 @@ function Sustainability() {
         </div>
       </Container>
 
-      <div className="fixed bottom-4 right-4 z-50 sm:hidden">
+      <div className="fixed bottom-4 right-4 z-50 md:hidden">
         <button className="rounded-3xl bg-black px-4 py-2 text-white shadow-lg shadow-blue-500/50 transition-shadow duration-300 hover:shadow-xl hover:shadow-blue-500">
           <Link href="#contact" aria-label="Home">
             Contact Us
--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
         to: process.env.EMAIL, // Receiver address from environment variable
         subject: `Contact Form: ${subject}`, // Subject
         text: message, // Plain text
-        html: `<p><strong>Interest:</strong> ${interest}</p><p>${message}</p> <p>Contact details: ${email}</p>`, // HTML message
+        html: `<div style="font-family: Arial, sans-serif;line-height: 1.8;color: #333;background: #ffffff;padding: 20px;border-radius: 12px;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 8px 16px rgba(0, 0, 0, 0.1);max-width: 500px;margin: 0 auto;"><h2 style="color: #007BFF; margin-bottom: 10px;">Interest</h2><p style="font-size: 18px; color: #444; margin-bottom: 15px;">${interest}</p><h3 style="color: #28A745; margin-bottom: 10px;">Message</h3><p style="font-size: 16px; color: #555; margin-bottom: 15px;">${message}</p><div style="padding-top: 15px; border-top: 2px solid #eee; margin-top: 15px;"><h3 style="color: #E63946; margin-bottom: 10px;">Contact Details</h3><p style="font-size: 16px;"><a href="mailto:${email}" style="color: #007BFF; text-decoration: none; font-weight: bold;">${email}</a></p></div></div>`,
       })
 
       res.status(200).json({ message: 'Email sent successfully!' })
